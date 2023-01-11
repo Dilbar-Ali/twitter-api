9e15442400f087ea1d9af91ea9efaafa9d305f15
Dilbar-ali <dilbarali0013@gmail.com>
2023-01-11 17:48:24 +0500

    Navbar complete

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,16 +1,35 @@
-import React from 'react'
-import './Navbar.css';
-import {Menubar} from 'primereact/menubar';
+import React from "react";
+import "./Navbar.css";
 import { InputSwitch } from 'primereact/inputswitch';
-const Navbar = () => {
-const [value, setValue] = React.useState(false);
-  return (
-    <>
-    <Menubar className='menu' end={<InputSwitch  checked={value} onChange={(e) => setValue(e.value)}/>} id='menu' start= { <i className="pi pi-twitter" style={{'fontSize': '2.3em','color':'white'}} >Twitter</i>} />
-    </>
- 
+function Navbar() {
+    const [value, setValue] = React.useState(false);
 
-  )
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <div className="container">
+          <a href="/" className="brand-logo">
+            <i className="pi pi-twitter" style={{ fontSize: "3em",'color':'#0daeee' }}></i>
+          </a>
+          <h1>Twitter </h1>
+        </div>
+        <div className="container1">
+          <ul  className="nav-link">
+            <li>
+              User
+            </li>
+            <li>
+              
+                <i className="pi pi-user" style={{ fontSize: "1.5em",'marginRight':'1rem'}}></i>
+              
+<InputSwitch checked={value} onChange={(e) => setValue(e.value)} style={{ fontSize: "2em",'color':'black',"borderRadius":"10rem"}} />
+ 
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
